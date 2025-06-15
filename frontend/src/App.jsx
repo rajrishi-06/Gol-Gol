1fd3cd6cd7b9aae2502a85319b7f4dacce5f716e
vaddethrishank <thrishank2005@gmail.com>
2025-06-15 12:17:04 +0530

    base code for using google api

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyMap from "../components/map";
+
+function App() {
+  return (
+    <div>
+      <h1>Gol-Gol Map</h1>
+      <MyMap />
+    </div>
+  );
+}
+
+export default App;
