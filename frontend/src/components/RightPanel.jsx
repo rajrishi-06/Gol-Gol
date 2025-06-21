00af0f5d08c144b177de8b99b8980d1bbdb621b3
vaddethrishank <thrishank2005@gmail.com>
2025-06-22 00:27:17 +0530

    google map showing wrong location need to resolve

--- frontend/src/components/RightPanel.jsx
+++ frontend/src/components/RightPanel.jsx
@@ -1,19 +1,23 @@
 import React from "react";
+import GoogleMapComponent from "./GoogleMapComponent";
 
 function RightPanel() {
     return(
         <>
             <div className="hidden sm:block flex-1 h-screen relative">
-                <img
+
+                 {/* <img
                 src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?fit=crop&w=1200&q=80"
                 alt="Promo"
                 className="w-full h-full object-cover"
                 />
                 <div className="absolute top-1/3 left-8 text-white">
-                <h2 className="text-3xl font-bold">Everyday city commute</h2>
+               <h2 className="text-3xl font-bold">Everyday city commute</h2>
                 <p className="mt-2 text-lg">Affordable AC cab rides at your doorstep</p>
-                <p className="mt-2 text-yellow-400 font-semibold">#OlaForWeb</p>
-                </div>
+                <p className="mt-2 text-yellow-400 font-semibold">#OlaForWeb</p> 
+                </div> */}
+                <GoogleMapComponent/> 
+                
             </div>
         </>
     );
